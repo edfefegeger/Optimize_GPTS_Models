e2332a52b66ea0fb1ab6269755018c8c68c0bf02
MYXAMOR338 <silova155@gmail.com>
2023-11-05 20:47:42 +0300

    some changes with save 1 file - 1 token something likw this

--- app.py
+++ app.py
@@ -7,25 +7,7 @@ from multiprocessing import Value
 input_file = "input.txt"
 output_template = "output_{}.csv"
 api_keys = [
-    "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",
-    "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",
-    "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",
-    "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",
-    
-    "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",
-    
-    "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",
-    
-    "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",
-    
-    "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",
-    
-    "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",
-    
-    "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",
-    
     "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",
-    
 ]
 
 model_engine = "gpt-3.5-turbo"
@@ -37,15 +19,15 @@ def split_data(data, n):
     avg = avg if avg > 0 else 1
     return [data[i:i + avg] for i in range(0, len(data), avg)]
 
-def save_progress(question_index, section_index):
+def save_progress(question_index, section_index, output_file_index):
     with open("progress.txt", "w") as progress_file:
-        progress_file.write(f"{section_index}:{question_index}")
+        progress_file.write(f"{section_index.value}:{question_index}:{output_file_index}")
 
-def worker(api_key, lines, output_file, start_index=0, section_index=0):
+def worker(api_key, lines, output_file_index, start_index=0, section_index=0):
+    output_file = output_template.format(output_file_index)
     with open(output_file, mode="w", encoding='utf-8') as w_file:
         file_writer = csv.writer(w_file, delimiter=",", quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
         file_writer.writerow(["Вопрос", "Ответ"])
-        num_for = 1
         for i, line in enumerate(lines, start=start_index):
             if i < section_index.value:
                 continue  # Пропустить вопросы до последней обработанной секции
@@ -64,30 +46,32 @@ def worker(api_key, lines, output_file, start_index=0, section_index=0):
                     answer = response.choices[0].message['content'].strip()
                     file_writer.writerow([line, answer])
                     print("Ответ:", answer)
-   
                 else:
                     print("Ошибка в полученном ответе.")
             except Exception as e:
                 print("Ошибка:", e)
                 if "exceeded your current quota" in str(e):
-                    print("Лимит токенов исчерпан. Берем следующий ключ.")
-                    break
+                    print("Лимит токенов исчерпан. Переход к следующему файлу.")
+                    section_index.value += 1
+                    save_progress(i, section_index, output_file_index)  # Сохранить прогресс
+                    return
                 else:
                     time.sleep(wait_error)
             finally:
                 section_index.value = i  # Обновить значение секции после каждого обработанного вопроса
-                save_progress(i, section_index.value)  # Сохранить прогресс
 
 if __name__ == "__main__":
     try:
         progress_data = open("progress.txt", "r").readline().strip().split(":")
         section_index = Value('i', int(progress_data[0]))
         question_index = int(progress_data[1])
-        print(f"Продолжаем с секции: {section_index.value}, вопроса: {question_index}")
+        output_file_index = int(progress_data[2])
+        print(f"Продолжаем с секции: {section_index.value}, вопроса: {question_index}, файл: {output_file_index}")
     except Exception as e:
         print("Ошибка при загрузке прогресса:", e)
         section_index = Value('i', 0)
         question_index = 0
+        output_file_index = 0
 
     with open(input_file, 'r', encoding='UTF-8') as file:
         lines = [line.rstrip() for line in file]
@@ -96,8 +80,7 @@ if __name__ == "__main__":
 
     processes = []
     for i, (api_key, data_split) in enumerate(zip(api_keys[section_index.value:], data_splits[section_index.value:]), start=section_index.value):
-        output_file = output_template.format(i)
-        p = multiprocessing.Process(target=worker, args=(api_key, data_split, output_file, question_index, section_index))
+        p = multiprocessing.Process(target=worker, args=(api_key, data_split, output_file_index + i, question_index, section_index))
         processes.append(p)
         p.start()
 
